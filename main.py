ef008d672ca146764a8b70afa366ffbe3c497777
Benjamin Capetillo <capetillobenjamin85@gmail.com>
2025-02-20 16:34:47 -0500

    Adjusts screen size

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class SoftwareRender:
 
     def __init__(self):
         pg.init()
-        self.RES = self.WIDTH, self.HEIGHT = 1600, 900
+        self.RES = self.WIDTH, self.HEIGHT = 1200, 600
         self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT // 2
         self.FPS = 60
         self.screen = pg.display.set_mode(self.RES)
